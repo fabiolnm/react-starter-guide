1a57f09d730860899834fc3eaa13ae82cb9515f5
Fabio Miranda <fabio@miranti.net.br>
2022-08-18 15:06:55 -0300

    Log click event

--- src/square.jsx
+++ src/square.jsx
@@ -1,6 +1,9 @@
 export default function Square({ value }) {
   return (
-    <button className="square">
+    <button
+      className="square"
+      onClick={() => console.log('click')}
+    >
       {value}
     </button>
   )
